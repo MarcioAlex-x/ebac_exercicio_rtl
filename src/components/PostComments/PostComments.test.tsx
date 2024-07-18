45bc1119484f85f30a4a5d8a5d67482b4b9a0774
MarcioAlex-x <marcioalex586@gmail.com>
2024-07-18 12:30:06 -0300

    Correções feitas

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,30 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import PostComments from '.';
 
-describe('Teste para o componente PostComment', () => {
+describe('Teste para o componente PostComments', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComments />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários corretamente', () => {
+        render(<PostComments />);
+        
+        const commentInput = screen.getByRole('textbox', { name: '' }) as HTMLTextAreaElement;
+        const commentButton = screen.getByRole('button', { name: 'Comentar' });
+
+        // Adicionando o primeiro comentário
+        fireEvent.change(commentInput, { target: { value: 'Primeiro comentário' } });
+        fireEvent.click(commentButton);
+
+        // Adicionando o segundo comentário
+        fireEvent.change(commentInput, { target: { value: 'Segundo comentário' } });
+        fireEvent.click(commentButton);
+
+        // Verificando se ambos os comentários foram renderizados
+        expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+        expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
     });
 });
